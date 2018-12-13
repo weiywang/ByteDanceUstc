c0f662ceaa206e81217839f10f1635c63de86a11
weiywang <weiywang@cisco.com>
2018-12-13 16:20:55 +0800

    First Blood 1.0

--- src/main/java/ArrayList/ThreeNumber.java
+++ src/main/java/ArrayList/ThreeNumber.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ThreeNumber {
-    public List<List<Integer>> threeSum(int[] nums) {
+    public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         for(int i = 0;i < nums.length;i++){
             for(int j = 0;j < i;j++){
@@ -17,5 +17,11 @@ public class ThreeNumber {
             }
         }
         return result;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = threeSum(nums);
+        System.out.println(result);
     }
 }
