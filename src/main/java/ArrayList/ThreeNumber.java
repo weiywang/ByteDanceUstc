970ae3bcb78cbce9483f11d9e39835ac74e38569
weiywang <weiywang@cisco.com>
2018-12-18 22:44:56 +0800

    Three sum success 1.0

--- src/main/java/ArrayList/ThreeNumber.java
+++ src/main/java/ArrayList/ThreeNumber.java
@@ -7,13 +7,22 @@ import java.util.List;
 public class ThreeNumber {
     public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        for(int i = 0;i < nums.length;i++){
-            for(int j = 0;j < i;j++){
-                for(int t = 0;t < j;t++) {
-                    if (nums[i] + nums[j] + nums[t]== 0) {
-                        result.add(Arrays.asList(nums[i], nums[j], nums[t]));
-                    }
+        Arrays.sort(nums);
+        if(nums.length == 0 || nums[0] > 0 || nums[nums.length-1] < 0) return result;
+        for(int k = 0;k < nums.length;++k){
+            if(nums[k] > 0) break;
+            if(k > 0 && nums[k] == nums[k-1]) continue;
+            int target = 0 - nums[k];
+            int i = k + 1, j = nums.length - 1;
+            while(i < j){
+                if(nums[i] + nums[j] == target){
+                    result.add(Arrays.asList(nums[k], nums[i], nums[j]));
+                    while(i < j && nums[i] == nums[i+1]) ++i;
+                    while(i < j && nums[j] == nums[j-1]) --j;
+                    ++i; --j;
                 }
+                else if(nums[i] + nums[j] < target) ++i;
+                else --j;
             }
         }
         return result;
