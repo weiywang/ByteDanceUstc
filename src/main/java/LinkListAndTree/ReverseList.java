a6107aa3ad213c9dde132b2e7419e85988e59ff7
weiywang <sa517370@mail.ustc.edu.cn>
2019-06-18 21:47:19 +0800

    ReverseList success 1.0

---
+++ src/main/java/LinkListAndTree/ReverseList.java
@@ -0,0 +1,29 @@
+package main.java.LinkListAndTree;
+
+public class ReverseList {
+    public static ListNode reverseList(ListNode head){
+        ListNode reverseHeadNode = null;
+        ListNode nowNode = head;
+        ListNode prevNode = null;
+        while(nowNode != null){
+            ListNode nextNode = nowNode.next;
+            if(nextNode == null){
+                reverseHeadNode = nowNode;
+            }
+            nowNode.next = prevNode;
+            prevNode = nowNode;
+            nowNode = nextNode;
+        }
+        return reverseHeadNode;
+    }
+
+    public static void main(String[] Args){
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        ListNode reverseHead = reverseList(head);
+        while(reverseHead != null){
+            System.out.println(reverseHead.val);
+            reverseHead = reverseHead.next;
+        }
+    }
+}
