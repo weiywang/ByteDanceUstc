66f6f6ef46379963da3127978e5b8d53f996be81
wangweiya <sa517370@mail.ustc.edu.cn>
2019-07-19 13:49:23 +0800

    MergeKLists completed 1.0

---
+++ src/main/java/LinkListAndTree/MergeKLists.java
@@ -0,0 +1,34 @@
+package LinkListAndTree;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class MergeKLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null){
+            return null;
+        }
+
+        Queue<ListNode> queue = new PriorityQueue<>(Comparator.comparing(o->o.val));
+
+        for(ListNode node: lists){
+            while(node != null){
+                queue.add(node);
+                node = node.next;
+            }
+        }
+
+        ListNode head = new ListNode(-1);
+        ListNode res = head;
+        ListNode cur;
+
+        while((cur = queue.poll()) != null){
+            head.next = cur;
+            head = head.next;
+        }
+
+        head.next = null;
+        return res.next;
+    }
+}
