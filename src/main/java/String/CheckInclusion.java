4acd5b6c18362b93e76f9f82c17a70a73aeea0ef
wangweiya <sa517370@mail.ustc.edu.cn>
2019-08-26 14:03:04 +0800

    review 1.0

--- src/main/java/String/CheckInclusion.java
+++ src/main/java/String/CheckInclusion.java
@@ -15,6 +15,7 @@ public class CheckInclusion {
 
         for(int i = 0; i < lengthTwo; i++){
             if(i >= lengthOne){
+                //为了维持滑动窗口的大小为lengthOne, 需要把之前加入的东西去掉
                 --charTwo[s2.charAt(i - lengthOne) - 'a'];
             }
             charTwo[s2.toCharArray()[i] - 'a']++;
--- src/main/java/String/LongestSubStr.java
+++ src/main/java/String/LongestSubStr.java
@@ -1,14 +1,19 @@
 package main.java.String;
 
+import java.util.*;
+
 public class LongestSubStr {
 	public static int longestSubString(String s){
-        int[] m = new int[256];
-        char[] ss = s.toCharArray();
-        int res = 0, left = 0;
-		for(int i = 0; i < ss.length; i++){
-			if(m[ss[i]] == 0 || m[ss[i]] < left) res = res > i-left+1 ? res : i-left+1;
-			else left = m[ss[i]];
-			m[ss[i]] = i+1;
+		int n = s.length();
+		int res = 0;
+		int end=0,start=0;
+		Map<Character,Integer> map=new HashMap<>();
+		for(;start<n && end<n;end++){
+			if(map.containsKey(s.charAt(end))){
+				start=Math.max(map.get(s.charAt(end)),start);//从有重复的下一个位置继续找
+			}
+			map.put(s.charAt(end),end+1);//map每次更新
+			res=Math.max(res,end-start+1);//结果每次更新
 		}
 		return res;
 	}	
